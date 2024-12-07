ed9a7f63346e9fa3a12afa146670d319c114e936
lauren <reyuqt01@gmail.com>
2024-12-06 21:09:02 -0500

    🚀 feat: add login route to the application
    - Updated the main routing in App.js to include a login page route.
    - Changed the default route from redirecting to the registration page
      to displaying the home page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Inventory from "./pages/Inventory";
 import Register from "./pages/Register";
-import { Navigate } from "react-router-dom";
+import Login from "./pages/Login";
 
-
-// Add a route for the registration page
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/register" />}  />
+        <Route path="/" element={<Home />} />
         <Route path="/inventory" element={<Inventory />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} /> {/* Login Route */}
       </Routes>
     </Router>
   );
 }
-
 export default App;
