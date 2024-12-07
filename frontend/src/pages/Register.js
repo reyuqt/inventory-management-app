f4ab0a80541eba1ed38a6b5471419b2ca19ea11c
lauren <reyuqt01@gmail.com>
2024-12-06 21:28:24 -0500

    :art: refactor(Register): enhance layout and styling of registration page

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,12 +1,20 @@
 import React from "react";
 import RegisterUser from "../components/RegisterUser";
+import { Container, Typography, Box } from "@mui/material";
 
 function Register() {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <Container maxWidth="sm" sx={{ mt: 8 }}>
+      <Box sx={{ textAlign: "center", mb: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Register Page
+        </Typography>
+        <Typography variant="subtitle1">
+          Create a new account to get started.
+        </Typography>
+      </Box>
       <RegisterUser />
-    </div>
+    </Container>
   );
 }
 
