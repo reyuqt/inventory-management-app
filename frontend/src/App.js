086f5e86b0f4f7ffe4262a34b04e3c36948d1a4e
lauren <reyuqt01@gmail.com>
2024-12-06 20:50:33 -0500

    ✨ feat(app): add routing for Home, Inventory, and Register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Inventory from "./pages/Inventory";
+import Register from "./pages/Register";
+import { Navigate } from "react-router-dom";
 
+
+// Add a route for the registration page
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/register" />}  />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
