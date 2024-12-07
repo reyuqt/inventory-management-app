6d5a64abe118077656d5845c78515b9c162bf50f
lauren <reyuqt01@gmail.com>
2024-12-06 20:51:35 -0500

    :heavy_plus_sign: feat(frontend): add Register page and user registration component
    :heavy_plus_sign: docs(backend): update README with docker-compose commands

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RegisterUser from "../components/RegisterUser";
+
+function Register() {
+  return (
+    <div>
+      <h1>Register Page</h1>
+      <RegisterUser />
+    </div>
+  );
+}
+
+export default Register;
